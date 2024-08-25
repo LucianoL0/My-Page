a4fc156297019c14d1f2021e3b25df8a55eb5485
Luciano lopes <luciano19lopes@gmail.com>
2024-08-25 11:42:21 -0300

    new content

--- Assets/JS/Scripty.js
+++ Assets/JS/Scripty.js
@@ -1,1 +1,9 @@
-// alert('Olá!');
+const firstLetter = document.querySelectorAll('.header-sc1');
+const colorH = getComputedStyle(document.body);
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    if (el.classList.contains('dark-buttom')) {
+        
+    }
+});
